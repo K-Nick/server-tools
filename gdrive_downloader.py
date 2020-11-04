f2790769963c996b836b9d925ab90e838099d8df
K.Nick <kningtg@gmail.com>
2020-11-04 00:33:09 +0000

    fix gdrive_downloader

--- gdrive_downloader.py
+++ gdrive_downloader.py
@@ -1,7 +1,8 @@
 import sys
 s = sys.argv
 FILE_ID = s[1]
-cmd = f"wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id={FILE_ID}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id={FILE_ID}\" -O filename && rm -rf /tmp/cookies.txt"
+FILE_NAME = s[2]
+cmd = f"wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id={FILE_ID}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id={FILE_ID}\" -O {FILE_NAME}\nrm -rf /tmp/cookies.txt"
 
 print(cmd)
 
